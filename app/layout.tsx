ea052f707925500f1b3da8a25a1c8867fea2dfb0
Giovanni Viviani <giovanniviviani@MacBook-Air-de-Giovanni.local>
2025-09-02 16:37:45 -0300

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,39 +1,27 @@
-// app/layout.tsx
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import { Analytics } from "@vercel/analytics/next"
 
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { UpdateBanner } from "@/components/layout/UpdateBanner"; // Importe o banner
-
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "duomed",
-  description: "Estude e teste os seus conhecimentos de forma divertida.",
-};
+  title: 'Duomed - Quiz Gamificado',
+  description: 'Uma plataforma gamificada para aprender medicina de forma eficaz e divertida.',
+  manifest: '/manifest.json'
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="pt">
-      <head>
-        <meta name="application-name" content="duomed" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="QuizApp" />
-        <meta name="format-detection" content="telephone=no" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="theme-color" content="#FFFFFF" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png"></link>
-      </head>
+    <html lang="pt-BR">
       <body className={inter.className}>
         {children}
-        <UpdateBanner /> {/* Adicione o banner aqui, no final do body */}
+        <Analytics />
       </body>
     </html>
-  );
+  )
 }
